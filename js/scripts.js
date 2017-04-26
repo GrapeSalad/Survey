991ac9c08a2c99c0854b4c86548b3a0246fd040c
David Wilson and Jordan Loop <david@>
2017-04-26 16:28:36 -0700

    working with date and age verification

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
+//BUSINESS LOGIC
+var legalAge = 2017 - 21;
+var orderSuccess = ageVerification >= legalAge;
+// orderSuccess is not gonna work like it is, need check IF greater than legalAge orderSuccess will return true, else false
+
+//INTERFACE LOGIC
 $(document).ready(function() {
   $("form#beerService").submit(function(event) {
     event.preventDefault();
     var beerSelection = $("#beerType").val();
     var beerPackaging = $("input:radio[name=packaging]:checked").val();
-    var ageVerification = $("#born").val();
+    var ageVerification = parseInt($("#born").val());
     console.log(beerSelection, beerPackaging, ageVerification);
   });
 });
